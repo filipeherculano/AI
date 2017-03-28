b5d06491efd2420b5faf81750c66c1f55f5e9d6e
filipeherculano <filipeherculanorocha@gmail.com>
2017-03-28 16:26:49 -0300

    agent improvement

--- ai/worlds/vacuum/ModelAgent.java
+++ ai/worlds/vacuum/ModelAgent.java
@@ -4,7 +4,7 @@ import ai.worlds.Location;
 import java.util.*;
 
 public class ModelAgent extends VacuumAgent{
-	public int hor = -1, ver = -1;
+	public int base = -1, altura = -1, state = -1;
 	public boolean trans = false;
 	public boolean lastRow = false;
 	
@@ -12,26 +12,87 @@ public class ModelAgent extends VacuumAgent{
 		Vector p = (Vector) percept;
 		if(p.elementAt(1) == "dirt") action = "suck";
 		else if(p.elementAt(0) == "bump"){
-			if(body.heading.x == 1 && body.heading.y == 0) {action = "turn left"; trans = true;}
-			else if(body.heading.x == -1 && body.heading.y == 0) {action = "turn right"; trans = true;}
-			else if(body.heading.x == 0 && body.heading.y == 1) {
-				if(lastRow == true) action = "shut-off";
-				else {
-					if(body.loc.x >= body.loc.y) action = "turn left";
-					else if(body.loc.x < body.loc.y) action = "turn right";
-					lastRow = true;
+			if(this.base == -1){
+				this.base = body.loc.x;
+				this.action = "turn left";
+			} else if(this.altura == -1){
+				this.altura = body.loc.y;
+				this.action = "turn left";
+				
+				if(this.altura % 2 == 0) this.state = 0;
+				else if(this.base % 2 == 0) this.state = 1;
+				else this.state = 2;
+				
+				if(this.altura % 2 != 0) this.trans = true;
+			} 
+		} else {
+			if(trans == true){
+				action = "forward";
+				trans = false;
+			} else {
+				switch(state){
+				case 0:
+					if(body.heading.x == 0 && body.heading.y == -1){
+						if(body.loc.x == 1) action = "turn left";
+						else action = "turn right";
+					}
+					else if(body.loc.forward(body.heading).x == base) {
+						action = "turn right";
+						trans = true;
+					}
+					else if(body.loc.forward(body.heading).x == 0) {
+						action = "turn left";
+						trans = true;
+					} else if(body.loc.x == 1 && body.loc.y == 1) action = "shut-off";
+					else action = "forward";
+					break;
+				case 1:
+					if(body.heading.x == -1 && body.heading.y == 0){
+						if(body.loc.y == 2) action = "turn right";
+						else action = "turn left";
+					}
+					else if(body.loc.forward(body.heading).y == 1) {
+						if(body.loc.x == 1) action = "forward";
+						else {
+							action = "turn right";
+							trans = true;
+						}
+					}
+					else if(body.loc.forward(body.heading).y > altura) {
+						action = "turn left";
+						trans = true;
+					} else if(body.loc.x == 1 && body.loc.y == 1) action = "shut-off";
+					else action = "forward";
+					break;
+				case 2:
+					if(body.heading.x == -1 && body.heading.y == 0){
+						if(body.loc.y == altura-1) action = "turn right";
+						else action = "turn left";
+					}
+					else if(body.loc.y == altura && body.heading.x == 0 && body.heading.y == 1) {
+						action = "turn left";
+						if(!(body.loc.x == 1 && body.loc.y == altura)) trans = true;
+					}
+					else if(body.loc.y == altura-1 && body.heading.x == 0 && body.heading.y == -1) {
+						if(body.loc.x == 1) {
+							action = "forward";
+							state = 0;
+						} else {
+							action = "turn right";
+							trans = true;
+						}
+					} else if(body.loc.x == 1 && body.loc.y == altura) {
+						if(!(body.heading.x == 0 && body.heading.y == -1)) action = "turn left";
+						else action = "forward";
+					}
+					else action = "forward";
+					break;
+					default: action = "forward";
 				}
 			}
-		} else {
-			if(body.heading.x == 0 && body.heading.y == 1) {
-				if(trans == true){
-					action = "forward";
-					trans = false;
-				} else {
-					if(body.loc.x > body.loc.y) action = "turn left";
-					else if(body.loc.x < body.loc.y) action = "turn right";
-				}
-			} else action = "forward";
+			
 		}
 	}
 }
+
+
